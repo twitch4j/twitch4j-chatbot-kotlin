b2778e42ec9a0216cdd0e06c45e732764ec2a67b
Sidd <iProdigy@users.noreply.github.com>
2022-09-30 18:13:16 -0700

    refactor: avoid deprecated PropertyNamingStrategy

--- src/main/kotlin/com/github/twitch4j/chatbot/kotlin/Configuration.kt
+++ src/main/kotlin/com/github/twitch4j/chatbot/kotlin/Configuration.kt
@@ -1,10 +1,10 @@
 package com.github.twitch4j.chatbot.kotlin
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Configuration(
     val debug: Boolean = false,
